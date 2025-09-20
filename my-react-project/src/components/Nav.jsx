b3a9cc65ec0eaa625e71eef60fc9c1c82acd1433
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-09-20 11:06:16 +0600

    Update

---
+++ my-react-project/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Home</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Nav;
