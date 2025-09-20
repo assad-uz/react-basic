be1cbc04f2d24c20d4359a1c3d5853ab67611f72
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-09-20 10:48:21 +0600

    Update

---
+++ my-react-project/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <div>
+                <h1>This is Home Page</h1>
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
