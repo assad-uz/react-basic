3beaa7289e4c59fcd9d2a266ad6698988184bbe5
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-09-21 11:24:47 +0600

    Update

--- my-react-project - 2/src/App.jsx
+++ my-react-project - 2/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import AboutUs from './pages/AboutUs';
 import ContactPage from './pages/ContactPage';
