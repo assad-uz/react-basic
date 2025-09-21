5401eb71e198d5cf436b1f684c04576dffee1535
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-09-21 12:52:30 +0600

    Update

--- react-website/src/App.jsx
+++ react-website/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div>
-       
+       <div>
+        <Navbar/>
+       </div>
     </div>
   );
 };
