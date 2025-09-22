46ca4e0545be3bfab986535561d37471051552a4
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-09-22 11:24:04 +0600

    Update

--- react-project-main/src/App.jsx
+++ react-project-main/src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Master from "./pages/Master";
+import AddUser from "./pages/users/AddUser";
+import ManageUser from "./pages/users/ManageUser";
 
 const App = () => {
   return (
     <div>
-      <h1>hello</h1>
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Master />} />
+            <Route path="/" element={<AddUser />} />
+            <Route path="/" element={<ManageUser />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 };
