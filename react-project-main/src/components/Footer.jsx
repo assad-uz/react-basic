078440cb03a838d619955254238c96aea5134671
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-09-23 09:51:18 +0600

    Update

--- react-project-main/src/components/Footer.jsx
+++ react-project-main/src/components/Footer.jsx
@@ -3,7 +3,19 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <h1>Footer Page</h1>
+            <footer className="main-footer">
+          <div className="float-right d-none d-sm-block">
+            <b>Version</b> 3.2.0
+          </div>
+          <strong>
+            Copyright © 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.
+          </strong>{" "}
+          All rights reserved.
+        </footer>
+        {/* Control Sidebar */}
+        <aside className="control-sidebar control-sidebar-dark">
+          {/* Control sidebar content goes here */}
+        </aside>
         </div>
     );
 };
